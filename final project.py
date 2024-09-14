cfac23b09e3e7f884a0f1255c8a55d37d4df2b7c
ARMAN-8718 <armanmazloom8@Gmail.com>
2024-09-14 15:44:58 +0330

    Add files via upload

---
+++ final project.py
@@ -0,0 +1,103 @@
+while True :
+    name=input("enter:")
+    if name =="start":
+        print("on")
+        break
+    else :
+        print("off")
+#jam dar khat 24 ast
+#menha dar khat 32 ast
+#zarb dar khat 38 ast
+#taqsim dar khat 45 ast
+#zarb taqsim menha jam dar khat 52 ast
+#zarb taqsim dar khat 59 ast
+#taqsim zarb menha dar khat 66 ast
+#jam menha dar khat 72 ast
+#menha taqsim dar khat 78 ast
+#taqsim jam dar khat 85 ast
+#zarb menha dar khat 92 ast
+#zarb jam dar khat 98
+
+
+
+        
+def jam(a,b):
+    
+    x=a+b
+    print(x)
+    
+jam(33,33)
+
+
+def menha(c,d):
+    x=c-d
+    print(x)
+
+menha(99,23)
+
+def zarb(g,h):
+    x=g * h
+    print(x)
+#ya det nare horof ha ro eshtebah nanevisi
+
+zarb(545,231)
+    
+def taqsim(k,l):
+
+    x=k / l
+    print(x)
+
+taqsim(87,5)
+
+def ztmj(h,a,g,q,r):
+
+    x=h * a / g-q+r
+    print(x)
+
+ztmj(566,121,90,10,466)
+
+def zarb_taqsim(g,h,t):
+
+    x=g * h / t
+    print(x)
+
+zarb_taqsim(34,54,34)
+
+def taqsim_zarb_menha(a,d,c,s):
+    x=a / d * c-s
+    print(x)
+
+taqsim_zarb_menha(214,4,121,222)
+
+def jam_menha(d,a,f):
+
+    x=d+a-f
+    print(x)
+
+jam_menha(131,34,122)
+def menha_taqsim(d,g,h):
+
+    x=d-g / h
+    print(x)
+
+menha_taqsim(345,123,55)
+
+def taqsim_jam(d,k,p):
+    
+
+    x=d / k+p
+    print(x)
+
+taqsim_jam(89,4,4464)
+def zarb_menha(h,d,s):
+
+    x=h * d-s
+    print(x)
+
+zarb_menha(67,576,76)
+def zarb_jam(g,j,o):
+
+    x=g * j+o
+    print(x)
+
+zarb_jam(76,566,54)
